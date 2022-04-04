a1ff06ec7c5bb6ee5681c76138a51fb644c4c65c
charlotte.russell <charlotte.russell@openknowledge.de>
2022-04-04 14:19:03 +0200

    first commit

---
+++ src/test/java/com/example/mitiappbackend/MiTiAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mitiappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiTiAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
