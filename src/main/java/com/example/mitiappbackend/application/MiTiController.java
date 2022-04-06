c586e7849c35b5337d42d68556abde2bf44cc4d6
marian.heck <marian.heck@openknowledge.de>
2022-04-06 17:06:24 +0200

    cors filter did a thing

--- src/main/java/com/example/mitiappbackend/application/MiTiController.java
+++ src/main/java/com/example/mitiappbackend/application/MiTiController.java
@@ -1,5 +1,6 @@
 package com.example.mitiappbackend.application;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MiTiController {
 
+
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
