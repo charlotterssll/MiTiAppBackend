8dcae992d15d1d4978c49674a0d0b172755abe14
charlotte.russell <charlotte.russell@openknowledge.de>
2022-04-06 09:17:15 +0200

    configure employees model and h2 test database

--- src/main/java/com/example/mitiappbackend/domain/Employees.java
+++ src/main/java/com/example/mitiappbackend/domain/Employees.java
@@ -1,7 +1,52 @@
-package com.example.mitiappbackend;
+package com.example.mitiappbackend.domain;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "EMPLOYEES")
 public class Employees {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "EMPLOYEEID")
+    private Long employeeID;
+
+    @Column(name = "FIRSTNAME")
+    private String firstName;
+
+    @Column(name = "LASTNAME")
+    private String lastName;
+
+    public Employees(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Employees() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employees{" +
+                "employeeID=" + employeeID +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
