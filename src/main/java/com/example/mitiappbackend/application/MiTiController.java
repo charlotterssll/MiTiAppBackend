81a06ce6c2b5360c7b136d946a1dec00bde1e6c3
charlotte.russell <charlotte.russell@openknowledge.de>
2022-04-07 17:24:55 +0200

    configure rest api to send test employee data

--- src/main/java/com/example/mitiappbackend/application/MiTiController.java
+++ src/main/java/com/example/mitiappbackend/application/MiTiController.java
@@ -1,17 +1,29 @@
 package com.example.mitiappbackend.application;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.mitiappbackend.domain.entities.Employee;
+import com.example.mitiappbackend.domain.entities.MiTi;
+import com.example.mitiappbackend.domain.entities.StringResponse;
+import com.example.mitiappbackend.domain.valueobjects.FirstName;
+import com.example.mitiappbackend.domain.valueobjects.LastName;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MiTiController {
 
-
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    public JSONObject hello() {
+        JSONObject obj = new JSONObject();
+
+        obj.put("name", "foo");
+
+        return obj;
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/test")
+    public Employee getMiTI() {
+        return new Employee(new FirstName("Charlotte"), new LastName("Russell"));
     }
 }
---
+++ src/main/java/com/example/mitiappbackend/domain/entities/StringResponse.java
@@ -0,0 +1,13 @@
+package com.example.mitiappbackend.domain.entities;
+
+public class StringResponse {
+    private String response;
+
+    public StringResponse(String s) {
+        this.response = s;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+}
