c1c32371106560636f1f1c14c5c3741628179a94
charlotte.russell <charlotte.russell@openknowledge.de>
2022-06-29 08:48:38 +0200

    reimplementation of DirtiesContext in AuthApiTest

--- src/test/java/com/example/mitiappbackend/application/AuthApiTest.java
+++ src/test/java/com/example/mitiappbackend/application/AuthApiTest.java
@@ -20,18 +20,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.util.NestedServletException;
 
-import com.example.mitiappbackend.infrastructure.AbstractPersistenceTest;
-
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
 @SpringBootTest
 public class AuthApiTest {
