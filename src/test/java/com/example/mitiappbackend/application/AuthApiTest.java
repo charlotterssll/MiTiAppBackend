b03209edaefe44ad9ef3b43dda9d166931070a12
charlotte.russell <charlotte.russell@openknowledge.de>
2022-06-29 08:40:46 +0200

    removal of DirtiesContext in AuthApiTest

--- src/test/java/com/example/mitiappbackend/application/AuthApiTest.java
+++ src/test/java/com/example/mitiappbackend/application/AuthApiTest.java
@@ -27,13 +27,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.util.NestedServletException;
 
 import com.example.mitiappbackend.infrastructure.AbstractPersistenceTest;
 
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
 @SpringBootTest
 public class AuthApiTest extends AbstractPersistenceTest {
